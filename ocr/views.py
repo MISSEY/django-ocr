a85bb77f7cc53a6e5430739a9582bd863386e6c0
Saurabh Mishra <skmishra@meta-objects.net>
2020-11-17 20:25:57 +0100

    Test 7

--- ocr/views.py
+++ ocr/views.py
@@ -4,7 +4,7 @@ from django.core.files.storage import FileSystemStorage
 
 from django.conf import settings
 import os
-import cv2
+#import cv2
 #from .ocr_image import app
 import shutil
 
@@ -12,10 +12,11 @@ def index(request):
     return render(request, 'ocr/detail.html')
 
 
-def load_image(filename):
-    img = cv2.imread(str(os.path.join(settings.BASE_DIR,'images')) + filename)
-    return img
-
+# def load_image(filename):
+#     #img = cv2.imread(str(os.path.join(settings.BASE_DIR,'images')) + filename)
+#     img = None
+#     return img
+#
 def delete_image(filename):
     os.remove(str(os.path.join(settings.BASE_DIR,'images')) + filename)
 
@@ -26,8 +27,9 @@ def upload_image(request):
         fs = FileSystemStorage(location=os.path.join(settings.BASE_DIR,'images'))
         filename = fs.save(image.name, image)
         uploaded_file_url = fs.url(filename)
-        img = load_image(uploaded_file_url)
-        model = os.path.join(settings.BASE_DIR, 'model')
+        #img = load_image(uploaded_file_url)
+        #model = os.path.join(settings.BASE_DIR, 'model')
+        #tessdata_dir_config = r'--tessdata-dir ' + str(model)
         #uuid = app.main(img)
         uuid = None
         delete_image(uploaded_file_url)
